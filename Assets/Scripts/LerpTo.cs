fd82fdd55c184e52f180aecf275be77d11fb3551
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-10-29 17:15:06 -0300

    Added LerpTo component to scene camera

---
+++ Assets/Scripts/LerpTo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTo : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        var position = Vector2.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
+    }
+}
