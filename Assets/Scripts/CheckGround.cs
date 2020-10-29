46e27bd41e7bedd46a88a9b23f8f20825bd1dc0f
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-10-29 16:24:26 -0300

    Copied project files;

---
+++ Assets/Scripts/CheckGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public Player player;
+
+    void Start(){
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.gameObject.GetComponent<Collider2D>());
+    }
+
+    void FixedUpdate(){
+        transform.position = new Vector2(player.gameObject.transform.position.x, player.gameObject.transform.position.y);
+    }
+    
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.tag == "Ground"){
+            player.SetOnGround(true)
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D col){
+        if (col.gameObject.tag == "Ground"){
+            player.SetOnGround(false);
+        }
+    }
+
+}
+
+// Encontrar o que está faltando neste script
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+
+    Rigidbody2D body;
+    bool onGround = false;
+    int score;
+    [SerializeField] private Text scoreHUD;
+
+
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove(){  
+        body.velocity = new Vector2(moveSpeed * Time.fixedDeltaTime * Input.GetAxisRaw("Horizontal"),
+                                                body.velocity.y);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && onGround)
+        {
+            // Implementar pulo do personagem
+            
+        }
+    }
+
+    public void SetOnGround(bool grounded){
+        if (grounded){
+            onGround = true;
+        } else{
+            onGround = false;
+        }
+    }
+
+// Completar o script abaixo
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.tag == "Coin"){
+            score ++;
+        }
+    }
+    
+void OnCollisionEnter2D(Collision2D col)
+{
+    if (col.gameObject.tag == "Ground")
+    {
+        SetOnGround(true);
+    }
+}
+
+void OnCollisionExit2D(Collision2D col)
+{
+    if (col.gameObject.tag == "Ground")
+    { 
+        SetOnGround(false);
+    } 
+}
+
+}
