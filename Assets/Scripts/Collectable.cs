b74eda2adf947ba9bec928753b8d75e301b2a7ec
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-10-30 07:39:35 -0300

    Added Collectable component to coins;

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public class OnCollectArgs { public Collectable collectable; public GameObject collector; }
+    public static event EventHandler<OnCollectArgs> OnCollect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnCollect?.Invoke(this, new OnCollectArgs() { collectable = this, collector = collision.gameObject });
+        Destroy(gameObject);
+    }
+}
