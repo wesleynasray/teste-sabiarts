65e892e9f2c3d022b03594a94e7fa651d90d9189
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-10-30 08:56:25 -0300

    Added GameOverModal and Game events;

---
+++ Assets/Scripts/GameOverModal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverModal : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+
+    private void Awake()
+    {
+        Game.Instance.OnGameOver += OnGameOver;
+    }
+
+    private void OnGameOver(object sender, System.EventArgs e)
+    {
+        content.SetActive(true);
+    }
+}
