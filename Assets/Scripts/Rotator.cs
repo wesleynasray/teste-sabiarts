4ebc81a3a4a6c8f38895d6a9f4e440680a2bc193
Wesley Raymundo <wesley.nasray@petitfabrik.com>
2020-10-30 07:47:02 -0300

    Added rotator to Coins sprite;

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 speed = Vector3.up * 300;
+
+    private void Update()
+    {
+        transform.eulerAngles = transform.eulerAngles + speed * Time.deltaTime;
+    }
+}
